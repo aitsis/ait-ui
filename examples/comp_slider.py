9d892756281128cc754d27585803c421c11a6c65
Hakan OZGUR <hakan.ozgur@gmail.com>
2023-07-13 10:28:36 +0300

    component class'i ve ornek slider eklendi

---
+++ examples/comp_slider.py
@@ -0,0 +1,26 @@
+#----------------------------------------------#
+#  AIT UI Component                             #
+#----------------------------------------------#
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from ait_ui.elements import Element , Text , Slider , Elm
+from ait_ui.component import Component
+
+
+class Comp_Slider(Component):
+    def __init__(self, label=None, id=None, min=0, max=100, value=50):
+        super().__init__()
+        with Element() as content:
+            content.style("display", "flex").style("justify-content", "space-between").style("align-items", "center").style("width", "100%")
+            Text(value = label)
+            Text(value = str(value), id = id + "-text")
+        Slider(id = id + "-slider", min = min, max = max, value=value).style("width", "100%").on("input", self.on_change_slider)
+
+    def on_change_slider(self, id, value):
+        print("Slider Changed", id, value)
+        Elm(id.split("-")[0] + "-text").value = value
+        if self.events["change_slider"]:
+            self.events["change_slider"](id, value)
+    
---
+++ examples/example_comp_slider.py
@@ -0,0 +1,24 @@
+#---------------------------------------------------------------#
+# This code added to run app.py from the examples directory
+# on production does not need to be added
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+#----------------------------------------
+
+
+from ait_ui import app
+from ait_ui.elements import Element
+from comp_slider import Comp_Slider
+
+def on_change_slider(id, value):
+    print("Slider Changed", id, value)
+
+with Element() as main:
+    slider1 = Comp_Slider(label="Slider 1", id="slider1", min=0, max=100, value=50)
+    slider2 = Comp_Slider(label="Slider 2", id="slider2", min=0, max=100, value=50)
+    slider1.on("change_slider", on_change_slider)
+    slider2.on("change_slider", on_change_slider)
+
+if __name__ == "__main__":
+    app.run(ui = main, debug=True)
