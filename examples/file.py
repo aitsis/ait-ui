ec30287c0560a870fc210a13022c6265ed3504fe
Hakan ÖZGÜR <hakan@ait.com.tr>
2023-07-17 23:09:38 +0300

    file.py example improved.

--- examples/file.py
+++ examples/file.py
@@ -12,24 +12,30 @@ from ait_ui.elements import Text
 from ait_ui.elements import Button
 from ait_ui.elements import File
 from ait_ui.elements import Form
-from ait_ui.elements import Label
+from ait_ui.elements import Label,Image
 
 def on_upload_done(file):
     if isinstance(file, str):
         print("File uploaded to:", file)
+        file_name = os.path.basename(file)
+        Elm("image1").value = "/assets/" + file_name
     else:
         print("File uploaded:", file.filename)
         print("File content_type:", file.content_type)
-        #get first 20 bytes of file
-        print("File content:", file.read(20))
-        file.seek(0)
-
-
-
+        
+    
+def on_file_input(id, value):
+    print("on_file_input", id, value)
+    
 with Element() as main:
     Label(id="dropzone1" , usefor="file").style("width", "100px").style("height", "100px")
-    file = File(id="file",on_upload_done=on_upload_done)
+    save_path = os.getcwd() + "/examples/assets"
+    Image(id = "image1")
+    file = File(id="file", save_path=save_path,on_upload_done=on_upload_done)
+    file.on("change",on_file_input)
 
 
 if __name__ == '__main__':
+    images_path = os.path.join(os.path.dirname(__file__), 'assets')
+    app.add_static_route('assets', images_path)
     app.run(ui = main, debug=True)
