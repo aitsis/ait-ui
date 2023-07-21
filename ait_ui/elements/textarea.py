b3523974207524ecd2d3eb8606e5cbe4d96dba61
semih <semiharslanhw@gmail.com>
2023-07-21 09:25:43 +0300

    components got changed , render method added to them

--- ait_ui/elements/textarea.py
+++ ait_ui/elements/textarea.py
@@ -3,7 +3,5 @@ class TextArea(Element):
     def __init__(self,id = None, value = None , placeholder = None):
         super().__init__(id, value)
         self.tag = "textarea"
-        self.value_name = "innerHTML"
-        self.has_content = True
         self.attrs["placeholder"] = placeholder
         
