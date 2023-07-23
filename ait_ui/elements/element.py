6e0655bb75f6aaf146595a9f093b72806822861a
Hakan ÖZGÜR <hakan@ait.com.tr>
2023-07-23 17:53:01 +0300

    element.py temizlendi script fonksiyonları eklendi

--- ait_ui/elements/element.py
+++ ait_ui/elements/element.py
@@ -1,6 +1,6 @@
 from .. import socket_handler
 from .. import Session
-
+from .. import index_gen
 def Elm(id):
     if id in Session.current_session.elements:
         return Session.current_session.elements[id]
@@ -28,14 +28,35 @@ class Element:
             self.cur_parent = self
         else:
             self.parent = self.cur_parent
-            self.cur_parent.add_child(self)
-        
+            self.cur_parent.children.append(self)
+    
+    # RUNTIME UPDATE ELEMENT ------------------------------------------------------------------------
     def update(self):
         Session.current_session.send(self.id, self.render(), "init-content")
+
+    # VALUE -----------------------------------------------------------------------------------------
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        self._value = value
+        Session.current_session.send(self.id, value, "change-"+self.value_name)
 
     def set_value(self, value):
         self.value = value
 
+    # GLOBAL PROPERTIES -------------------------------------------------------------------------------
+    @property
+    def webserver(self):
+        return socket_handler.web_server
+
+    @property
+    def web_request(self):
+        return socket_handler.web_request
+    
+    # SESSION PROPERTIES -------------------------------------------------------------------------------
     @property
     def cur_parent(self):
         return Session.current_session.current_parent
@@ -44,35 +65,44 @@ class Element:
     def cur_parent(self, value):
         Session.current_session.current_parent = value
 
-    @property
-    def value(self):
-        return self._value
+    # MESSAGING -------------------------------------------------------------------------------------
+    def send(self, id, value, event_name):
+        Session.current_session.send(id, value, event_name)
     
-    @property
-    def webserver(self):
-        return socket_handler.web_server
+    def queue_for_send(self, id, value, event_name):
+        Session.current_session.queue_for_send(id, value, event_name)
+    # SCRIPT PROPERTIES -----------------------------------------------------------------------------
+    def add_header_item(self, id, item):
+        if id in index_gen.header_items:
+            return
+        index_gen.header_items[id] = item
+        
+    def add_script_source(self, id, script):
+        if id in index_gen.scripts:
+            return
+        index_gen.script_sources[id] = script
 
-    @property
-    def web_request(self):
-        return socket_handler.web_request
+    def add_script(self, id, script):
+        if id in index_gen.scripts:
+            return
+        index_gen.scripts[id] = script
 
-    @value.setter
-    def value(self, value):
-        self._value = value
-        Session.current_session.send(self.id, value, "change-"+self.value_name)
+    def add_style(self, id, style):
+        if id in index_gen.styles:
+            return
+        index_gen.styles[id] = style
 
+    # RUNTIME JAVASCRIPT -------------------------------------------------------------------------------  
     def toggle_class(self, class_name):
-        Session.current_session.send(self.id, class_name, "toggle-class")
+        self.send(self.id, class_name, "toggle-class")
     
     def set_attr(self, attr_name, attr_value):
-        Session.current_session.send(self.id, attr_value, "change-"+attr_name)
+        self.send(self.id, attr_value, "change-"+attr_name)
     
     def set_style(self, attr_name, attr_value):
-        Session.current_session.send(self.id, attr_value, "set-"+attr_name)
+        self.send(self.id, attr_value, "set-"+attr_name)
 
-    def add_child(self, child):        
-        self.children.append(child)
-
+    # WITH ENTRY - EXIT -------------------------------------------------------------------------------
     def __enter__(self):                
         self.cur_parent = self
         self.children = []
@@ -84,6 +114,7 @@ class Element:
     def __str__(self):
         return self.render()
     
+    # RENDER -----------------------------------------------------------------------------------------
     def cls(self,class_name):
         self.classes.append(class_name)
         return self
@@ -92,52 +123,42 @@ class Element:
         self.styles[style] = value
         return self
     
+    # PYTHON EVENTS ----------------------------------------------------------------------------------
     def on(self,event_name,action):
         self.events[event_name] = action
         return self
     
+    # JAVASCRIPT EMIT --------------------------------------------------------------------------------
     def get_client_handler_str(self, event_name):
         return f" on{event_name}='clientEmit(this.id,this.{self.value_name},\"{event_name}\")'"
-
+    
+    # RENDER -----------------------------------------------------------------------------------------
     def render(self):
         str = f"<{self.tag}"
-        # <div
         if self.id is not None:
             str += f" id='{self.id}'"
-        # <div id='myid'   
         class_str = " ".join(self.classes)
-        # <div id='myid' class='myclass1 myclass2'
         if(len(class_str) > 0):
             str += f" class='{class_str}'"
-        # <div id='myid' class='myclass1 myclass2'
         if(len(self.styles) > 0):
             style_str = " style='"
-            # <div id='myid' class='myclass1 myclass2' style='
             for style_name, style_value in self.styles.items():
                 style_str += f" {style_name}:{style_value};"
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;'
             str += style_str + "'"
         for attr_name, attr_value in self.attrs.items():
             str += f" {attr_name}='{attr_value}'"
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value'
         for event_name, action in self.events.items():
             str += self.get_client_handler_str(event_name)
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value' onevent_name='clientEmit(this.id,this.value,"event_name")'
         if self.has_content:
             str +=">"
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value' onevent_name='clientEmit(this.id,this.value,"event_name")'>
             str +=f"{self.value if self.value is not None and self.value_name is not None else ''}"
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value' onevent_name='clientEmit(this.id,this.value,"event_name")'>value
             for child in self.children:
                 str += child.render()
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value' onevent_name='clientEmit(this.id,this.value,"event_name")'>value<child1><child2>
             str += f"</{self.tag}>"
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value' onevent_name='clientEmit(this.id,this.value,"event_name")'>value<child1><child2></div>
         else:
             if self.value is not None:
                 if(self.value_name is not None):
                     str +=f' {self.value_name} ="{self.value}"'
             str += "/>"
-            # <div id='myid' class='myclass1 myclass2' style='width:100px;height:100px;' attr_name='attr_value' onevent_name='clientEmit(this.id,this.value,"event_name")' value='value'/>
         return str
 
