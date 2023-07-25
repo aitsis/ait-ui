5d4a38f1834247ed86a65d3f36327de17ffaaf1e
semih <semiharslanhw@gmail.com>
2023-07-25 13:42:53 +0300

    autobinds deleted from elements

--- ait_ui/elements/accordion.py
+++ ait_ui/elements/accordion.py
@@ -4,8 +4,8 @@ from .text import Text
 global isAccordionOpen
 
 class Accordion(Element):
-    def __init__(self, id=None, value=None, auto_bind=True):
-        super().__init__(id, value, auto_bind)
+    def __init__(self, id=None, value=None):
+        super().__init__(id, value)
         self.tag = "div"
         self.value_name = "innerHTML"
         self.cls("accordion")
--- ait_ui/elements/video.py
+++ ait_ui/elements/video.py
@@ -1,7 +1,7 @@
 from .element import Element
 class Video(Element):
-    def __init__(self, id=None, value=None, auto_bind=True , src = None , controls = False , autoplay = None):
-        super().__init__(id, value, auto_bind)
+    def __init__(self, id=None, value=None, src = None , controls = False , autoplay = None):
+        super().__init__(id, value)
         self.tag = "video"
         self.value_name = "src"
         self.has_content = True
