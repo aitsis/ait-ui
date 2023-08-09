dceb0ca1323ba0badc60efea41ad818f42f5fe73
oguzhan <oguzhan@ait.com.tr>
2023-08-09 11:27:01 +0300

    Component class value setter override

--- ait_ui/component.py
+++ ait_ui/component.py
@@ -3,3 +3,11 @@ from ait_ui.elements import Element
 class Component(Element):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
