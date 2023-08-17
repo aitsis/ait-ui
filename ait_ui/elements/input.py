c830a120f25d0303209928851c07baca5f88b387
umut <umut@ait.com.tr>
2023-08-17 16:42:34 +0300

    Place holder bug fix

--- ait_ui/elements/input.py
+++ ait_ui/elements/input.py
@@ -1,7 +1,7 @@
 from ..core import Element
 
 class Input(Element):
-    def __init__(self, id=None, value=None, type="text", placeholder=None, step=None, autoBind=True):
+    def __init__(self, id=None, value=None, type="text", placeholder="", step=None, autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "input"
         self.value_name = "value"
