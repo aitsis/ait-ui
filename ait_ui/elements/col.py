108c8a154e4fac4f6bc435730711f4c091fb867a
umut <umut@ait.com.tr>
2023-08-17 16:42:10 +0300

    Col ve Row içerik ortalama kalktı

--- ait_ui/elements/col.py
+++ ait_ui/elements/col.py
@@ -3,4 +3,4 @@ from ..core import Element
 class Col(Element):
     def __init__(self,id = None, value = None, autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
-        self.classes.append("col container")
+        self.classes.append("col")
--- ait_ui/elements/row.py
+++ ait_ui/elements/row.py
@@ -3,4 +3,4 @@ from ..core import Element
 class Row(Element):
     def __init__(self,id = None, value = None, autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
-        self.classes.append("row container")
+        self.classes.append("row")
