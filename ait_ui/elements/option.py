084265d90ea7a5e24beb5bd2c61cb5ea3e99405d
umut <umut@ait.com.tr>
2023-08-17 16:43:03 +0300

    Text eklendi

--- ait_ui/elements/option.py
+++ ait_ui/elements/option.py
@@ -1,9 +1,10 @@
 from ..core import Element
 
 class Option(Element):
-    def __init__(self, id=None, value=None, autoBind=True):
+    def __init__(self, id=None, value=None, autoBind=True, text=""):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "option"
         self.value_name = "innerHTML"
         self.has_content = True
         self.attrs["value"] = value
+        self.value = text
