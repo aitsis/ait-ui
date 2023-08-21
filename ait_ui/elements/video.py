476eaf851fef075a449f32e4e20ab3d33dfd89f4
semih <semiharslanhw@gmail.com>
2023-08-21 21:08:55 +0300

    controls got removed from video element

--- ait_ui/elements/video.py
+++ ait_ui/elements/video.py
@@ -1,13 +1,12 @@
 from ..core import Element
 
 class Video(Element):
-    def __init__(self, id=None, value=None, src=None, loop="true", controls=False, autoplay="true", autoBind=True):
+    def __init__(self, id=None, value=None, src=None, loop="true", autoplay="true", autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "video"
         self.value_name = "src"
         self.has_content = True
         self.attrs["value"] = value
         self.attrs["src"] = src
-        self.attrs["controls"] = controls
         self.attrs["loop"] = loop
         self.attrs["autoplay"] = autoplay
