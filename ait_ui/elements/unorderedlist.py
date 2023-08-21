0acfcf5212f9e186ac7270602bc9f5eb931fdb75
semih <semiharslanhw@gmail.com>
2023-08-21 17:08:28 +0300

    role attr added to ul

--- ait_ui/elements/unorderedlist.py
+++ ait_ui/elements/unorderedlist.py
@@ -1,8 +1,9 @@
 from ..core import Element
 
 class UnorderedList(Element):
-    def __init__(self,id = None,value = None, autoBind=True):
+    def __init__(self,id = None,value = None,role = None, autoBind=True):
         super().__init__(id = id, value = value, autoBind=autoBind)
         self.tag = "ul"
         self.value_name = "innerHTML"
         self.has_content = True
+        self.attrs["role"] = role
