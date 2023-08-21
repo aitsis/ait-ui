cb26b264df00f7a18a264d0f3f133051632e2f8f
semih <semiharslanhw@gmail.com>
2023-08-21 17:06:10 +0300

    input get name attr

--- ait_ui/elements/input.py
+++ ait_ui/elements/input.py
@@ -1,13 +1,14 @@
 from ..core import Element
 
 class Input(Element):
-    def __init__(self, id=None, value=None, type="text", placeholder=None, step=None, required=False, autoBind=True):
+    def __init__(self, id=None, value=None, name=None, type="text", placeholder=None, step=None, required=False, autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "input"
         self.value_name = "value"
         self.has_content = False
         self.attrs["type"] = type
         self.attrs["placeholder"] = placeholder
+        self.attrs["name"] = name
 
         if step is not None:
             self.attrs["step"] = step
