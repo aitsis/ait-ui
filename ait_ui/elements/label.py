2745f77274bbbbbff3c7f5f4ed3f2e95c3dff62b
semih <semiharslanhw@gmail.com>
2023-08-21 17:08:10 +0300

    tabindex added label.py

--- ait_ui/elements/label.py
+++ ait_ui/elements/label.py
@@ -1,10 +1,11 @@
 from ..core import Element
 
 class Label(Element):
-    def __init__(self, id = None , usefor = None,value = None, autoBind = True):
+    def __init__(self, id = None, tabindex = None, usefor = None,value = None, autoBind = True):
         super().__init__(id= id , value = value, autoBind= autoBind)
         self.classes.append("label")
         self.usefor = usefor
         self.tag = "label"
         self.attrs["for"] = usefor
+        self.attrs["tabindex"] = tabindex
         self.has_content = True
