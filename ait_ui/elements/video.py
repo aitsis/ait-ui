bcfc31c8ca72da969b91162716c978ab9259e89d
oguzhan <oguzhan@ait.com.tr>
2023-08-29 21:56:12 +0300

    Video updated

--- ait_ui/elements/video.py
+++ ait_ui/elements/video.py
@@ -4,9 +4,9 @@ class Video(Element):
     def __init__(self, id=None, value=None, src=None, loop="true", autoplay="true", autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "video"
-        self.value_name = "src"
         self.has_content = True
         self.attrs["value"] = value
-        self.attrs["src"] = src
-        self.attrs["loop"] = loop
-        self.attrs["autoplay"] = autoplay
+        self.attrs["loop"] = "true"
+        self.attrs["autoplay"] = "true"
+        self.attrs["playsinline"] = "true"
+        self.attrs["muted"] = "true"
