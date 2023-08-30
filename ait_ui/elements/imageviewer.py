0dc6ed0a1d595569bfd5bd5fc8faeb4bc33c59af
oguzhan <oguzhan@ait.com.tr>
2023-08-30 14:08:46 +0300

    Updated viewers

--- ait_ui/elements/imageviewer.py
+++ ait_ui/elements/imageviewer.py
@@ -1,11 +1,12 @@
 from ..core import Element
 
 class ImageViewer(Element):
-    def __init__(self, id=None, value=None, hasButtons=True, autoBind=True):
+    def __init__(self, id=None, value=None, hasButtons=True, ableToZoom=False, autoBind=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "div"
         self.value_name = None
         self.hasButtons = hasButtons
+        self.ableToZoom = ableToZoom
         self.add_script_source('seadragon-js-lib', '<script src="https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.1.0/openseadragon.min.js"></script>')
         self.add_script_source('seadragon', '<script src="js/seadragon.js"></script>')
 
@@ -23,6 +24,8 @@ class ImageViewer(Element):
             self.queue_for_send(self.id, {"hasButtons": str(self.hasButtons).lower()}, "init-seadragon")
         if self.value is not None:
             self.queue_for_send(self.id, self.value_to_command("open",{"type": "image","url": self._value}), "seadragon")
+        if self.ableToZoom is not None:
+            self.queue_for_send(self.id, self.value_to_command("set-scroll-zoom", str(self.ableToZoom).lower()), "seadragon")
         return super().render()
 
     def closeImage(self):
@@ -33,7 +36,7 @@ class ImageViewer(Element):
         self.send(self.id, self.value_to_command("mouse-mode", value), "seadragon")
 
     def setScrollZoom(self, value):
-        self.send(self.id, self.value_to_command("set-scroll-zoom", value), "seadragon")
+        self.send(self.id, self.value_to_command("set-scroll-zoom", str(value).lower()), "seadragon")
 
     def brush_size(self, value):
         self.send(self.id, self.value_to_command("brush-size", value), "seadragon")
