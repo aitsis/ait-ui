05a212a8ee1f63af90d07214dec5719905e18489
oguzhan <oguzhan@ait.com.tr>
2023-08-30 13:51:40 +0300

    value_name changed to checked

--- ait_ui/elements/check.py
+++ ait_ui/elements/check.py
@@ -4,7 +4,7 @@ class Check(Element):
     def __init__(self,id = None,value = None, autoBind=True):
         super().__init__(id = id, value = value, autoBind=autoBind)
         self.tag = "input"
-        self.value_name = "value"
+        self.value_name = "checked"
         self.has_content = False
         self.attrs["type"] = "checkbox"
 
--- ait_ui/elements/radio.py
+++ ait_ui/elements/radio.py
@@ -4,7 +4,7 @@ class Radio(Element):
     def __init__(self,id = None,value = None,name = None, autoBind=True):
         super().__init__(id = id, value = value, autoBind=autoBind)
         self.tag = "input"
-        self.value_name = "value"
+        self.value_name = "checked"
         self.has_content = False
         self.attrs["type"] = "radio"
         self.attrs["name"] = name
