8952025aa4f43ca9f9c0dc0b7da207e67c2ec70c
oguzhan <oguzhan@ait.com.tr>
2023-08-30 13:52:32 +0300

    added tooltip

--- ait_ui/elements/link.py
+++ ait_ui/elements/link.py
@@ -1,9 +1,10 @@
 from ..core import Element
 
 class Link(Element):
-    def __init__(self,id = None, value = None, href = None, autoBind=True, target = ""):
+    def __init__(self,id = None, value = None, href = None, tooltip="",autoBind=True, target = ""):
         super().__init__(id = id, value = value, autoBind=autoBind)
         self.tag = "a"
         self.value_name = "innerHTML"
         self.attrs["href"] = href
         self.attrs["target"] = target
+        self.attrs["title"] = tooltip
