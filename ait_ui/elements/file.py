4746c1599c3b1ccb2ece8590ad9884108b826f70
oguzhan <oguzhan@ait.com.tr>
2023-08-31 17:21:08 +0300

    file updated

--- ait_ui/elements/file.py
+++ ait_ui/elements/file.py
@@ -17,12 +17,8 @@ class File(Element):
         self.style("display", "none")
         self.save_path = save_path
         self.events["file-upload-started"] = self.upload_started
-        self.events["change"] = self.on_change
+        self.events["change"] = lambda id, value: print("change", id, value)
         self.on_upload_done = on_upload_done
-        self.uploaded_file_path = None
-        self.uploaded_file_name = None
-        self.timeout = 10
-        self.counter = 0
 
     def get_client_handler_str(self, event_name):
         if event_name in ["input","change"]:
@@ -30,44 +26,30 @@ class File(Element):
         else:
             return super().get_client_handler_str(event_name)
 
-    def on_change(self,id,file):
-        pass
+    def upload_done(self, uploaded_file_path, uploaded_file_name):
+        try:
+            if self.save_path:
+                save_file_path = os.path.join(self.save_path, uploaded_file_name)
+                shutil.move(uploaded_file_path, save_file_path)
+                self.on_upload_done(save_file_path)
+        except Exception as e:
+            print(e)
 
-    def upload_done(self):
-        if self.save_path is not None:
-            save_file_path = os.path.join(self.save_path, self.uploaded_file_name)
-            shutil.copyfile(self.uploaded_file_path, save_file_path)
-            print("File saved to", save_file_path)
-            os.remove(self.uploaded_file_path)
-            self.on_upload_done(save_file_path)
-            #TODO: add error handling here
+    def upload_started(self, id, file):
+        print("upload_started", id, file["file_name"])
+        uploaded_file_path = os.path.join(tempfile.gettempdir(), file["uid"])
+        uploaded_file_name = file["file_name"]
 
-    def upload_started(self,id,file):
-        #timer = threading.Timer(1, self.upload_started, [id,file])
-        #print(f"{self.counter} {self.timeout}")
-        if self.counter == 0:
-            print("upload_started", id, file["file_name"])
-            self.uploaded_file_path = os.path.join(tempfile.gettempdir(), file["uid"])
-            self.uploaded_file_name = file["file_name"]
-
-        # self.counter += 1
-        # if self.counter > self.timeout:
-        #     self.counter = 0
-        #     self.uploaded_file_name = None
-        #     self.uploaded_file_path = None
-        #     print("upload timeout")
-        #     #TODO: add error handling here
-        #     return
-
-        if os.path.exists(self.uploaded_file_path):
+        if os.path.exists(uploaded_file_path):
             # check if the file is used by another process
             try:
-                with open(self.uploaded_file_path, "rb") as f:
+                with open(uploaded_file_path, "rb") as f:
                     f.close()
-                self.upload_done()
-                self.counter = 0
+                self.upload_done(uploaded_file_path, uploaded_file_name)
                 return
-            except:
+            except Exception as e:
+                print(e)
                 pass
-
-        # timer.start()
+        else:
+            print("File not found:", uploaded_file_path)
+            return -1
