659c28ba9c28ff8849ba7ff50757a7e594f31c2d
oguzhan <oguzhan@ait.com.tr>
2023-09-23 20:35:33 +0300

    updated image for lazy

--- ait_ui/elements/image.py
+++ ait_ui/elements/image.py
@@ -1,11 +1,17 @@
 from ..core import Element
 
 class Image(Element):
-    def __init__(self, id=None, value=None, autoBind=True):
+    def __init__(self, id=None, value=None, autoBind=True, lazy=True):
         super().__init__(id=id, value=value, autoBind=autoBind)
         self.tag = "img"
         self.value_name = "src"
         self.has_content = False
-        self.attrs["src"] = value if value else ""
+        if lazy:
+            self.attrs["data-src"] = value if value else ""
+            self.attrs["data-srcset"] = value if value else ""
+            self.attrs["data-sizes"] = "auto"
+            self.cls("lazyload")
+        else:
+            self.attrs["src"] = value if value else ""
         self.attrs["alt"] = "image"
         self.attrs["loading"] = "lazy"
