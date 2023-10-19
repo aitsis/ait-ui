2cc3a4f5818707b3ddb841af9a89a70bf863bfef
dlcompute <dlcompute@cn1-ob.aitws>
2023-10-19 14:27:49 +0300

    project name changeed

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ current_pkg_version = None
 
 with open("version.txt", "r") as f:
     current_pkg_version = f.read().strip()
-shutil.copyfile("version.txt", os.path.join(os.path.dirname(__file__), "ait-ui", "version.txt"))
+shutil.copyfile("version.txt", os.path.join(os.path.dirname(__file__), "aitsis-ui", "version.txt"))
 
 setup(
     
-    name="ait-ui",
+    name="aitsis-ui",
     version = current_pkg_version, 
     description = "AIT UI",
     author = "AIT",
     packages = find_packages(),
     package_data= {
-        "ait-ui": [ "static/*", "js/*", "version.txt" ]
+        "aitsis-ui": [ "static/*", "js/*", "version.txt" ]
     },
     exclude_package_data = {
-        "ait-ui": [ "*.pyc", "requirements.txt", "README.md" ]
+        "aitsis-ui": [ "*.pyc", "requirements.txt", "README.md" ]
     },
     python_requires = ">=3.6",
 
